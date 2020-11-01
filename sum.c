8ac9f2e857646526a994e6a34ad33c15fbd3c61e
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:55:56 +0530

    sum of numbers
    to find the sum of two numbers

---
+++ sum.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : sum.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("sum of two numbers \n"); /* prints  */
+	int a,b,sum;
+	a=24;
+	b=57;
+	sum=a+b;
+	printf("the value of a is %d \n",a);
+	printf("the value of b is %d \n",b);
+	printf("the sum of a and b  %d \n",sum);
+	return EXIT_SUCCESS;
+}
