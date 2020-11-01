1e9bda45e7be2b8d0935b4a9ea43b58b15a21947
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:44:54 +0530

    char printing
    program to print the character

---
+++ char print.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : char.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("character printing \n"); /* prints  */
+	char a;
+	a='V';
+	printf("the character is entered is  %c",a);
+	return EXIT_SUCCESS;
+}
