ed91b6f6080197a16700be134c32a85318444120
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:52:38 +0530

    multiple
    to find the multiple

---
+++ multiple.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : multiple.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("the multiple of two numbers \n"); /* prints  */
+	int a,b,m;
+	a=24;
+	b=7;
+	m=a*b;
+	printf("the value of a is %d \n",a);
+	printf("the value of b is %d \n",b);
+	printf("the multiple  of a and b  %d \n",m);
+	return EXIT_SUCCESS;
+}
