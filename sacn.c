6e12f728d0d1e7d3918afa9d5af0ce23148df9e6
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:54:33 +0530

    scan the number
    to scan the number from the user

---
+++ sacn.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : sacn.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	printf("enter the number :"); /* prints  */
+	scanf("%d",&a);
+	printf("you have entered is %d",a);
+	return EXIT_SUCCESS;
+}
