11bb1cf6719a57c01e4371cb5bb655b2d55ae8f4
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:56:58 +0530

    sum of numbers in float
    to find the sum of numbers in float

---
+++ sum in float.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : sum.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("the sum \n"); /* prints  */
+	int a=15;
+	float b=25.14;
+	float sum;
+	sum=a+b;
+	printf("the value of a is %d \n",a);
+	printf("the value of b is %f \n",b);
+	printf("the sum  of a and b is %f \n",sum);
+	return 0;
+}
