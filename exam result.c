5bd98971e30369a7fc370998521dd9453ff975dd
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-01 18:49:04 +0530

    result of the exam
    program to find the result of the exam

---
+++ exam result.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : exam.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("EXAM RESULT CHECKING \n"); /* prints  */
+	int m=23;
+	if(m<24)
+		printf("you have fail in the exam \n");
+	else
+		printf("you have pass the exam \n");
+	return EXIT_SUCCESS;
+}
