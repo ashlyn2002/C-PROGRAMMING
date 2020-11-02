36e2b5c78012d12653196b1293314790f169d95e
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-11-02 17:39:08 +0530

    string array
    printing the string using the array

---
+++ string array.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : string.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("printing the string using the array \n"); /* prints  */
+	char a[20]="ashlyn";
+	printf("the string is : %s \n",a);
+	return EXIT_SUCCESS;
+}
