728523e506c5bcda6816bb3f3c6950abf05214f2
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-12-13 18:32:26 +0530

    reverse the DLL
    create the DLL,display the DLL,count the number of elements i  the DLL and reverse the DLL

---
+++ reverse the DLL.c
@@ -0,0 +1,128 @@
+/*
+ ============================================================================
+ Name        : reverse.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void creat();
+void display();
+void count();
+void reverse();
+struct node
+{
+int data;
+struct node *next;
+struct node *pre;
+};
+struct node *head=0;
+int main()
+{
+    printf("function of double linked list \n");
+    printf("1.implementation of linked list\n");
+    printf("2.display the linked list\n");
+    printf("3.count the number of element in the linked list\n");
+    printf("4.revers the linked list\n");
+    printf("5.exit\n");
+    int choice;
+    printf("enter the choice =");
+    scanf("%d",&choice);
+    while(choice!=6)
+    {
+        switch(choice)
+        {
+            case 1: printf("implementation\n");
+            creat();
+            break;
+            case 2:printf("display the linked list\n");
+            display();
+            break;
+            case 3:printf("count the number of element in the linked list\n");
+            count();
+            break;
+            case 4:printf("revers the linked list\n");
+            reverse();
+            break;
+            case 5:printf("exit \n");
+            exit(0);
+            break;
+            default:printf("invalied choice \n");
+            break;
+        }
+         printf("enter the choice =");
+        scanf("%d",&choice);
+    }
+}
+void creat()
+{
+struct node *newnode,*temp;
+int choice;
+    while(choice)
+    {
+        newnode=(struct node *)malloc(sizeof(struct node));
+            printf("enter the data =");
+            scanf("%d",&newnode->data);
+            newnode->pre=0;
+            newnode->next=0;
+        if(head==0)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            newnode->pre=temp;
+            temp=newnode;
+        }
+        printf("do you need to continue(0,1)?\n enter 0 to stop  enter 1 to continue = ");
+        scanf("%d",&choice);
+    }
+}
+void display()
+{
+    struct node *temp;
+    temp=head;
+    if(head==0)
+    {
+        printf("the list is empty");
+    }
+    while(temp!=0)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+void count()
+{
+    struct node *temp;
+    temp=head;
+    int c=0;
+    while(temp!=0)
+    {
+        c++;
+        temp=temp->next;
+    }
+    printf("the number of elements in the linked list is %d \n",c);
+}
+void reverse()
+{
+    struct node *currentnode,*nextnode,*prenode;
+    prenode=0;
+    currentnode=nextnode=head;
+    while(nextnode!=0)
+    {
+        nextnode=nextnode->next;
+        currentnode->next=prenode;
+        currentnode->pre=nextnode;
+        prenode=currentnode;
+        currentnode=nextnode;
+    }
+    head=prenode;
+}
