0592560352e1e4567d99c9bb3158b8c92fbc5f47
Ashlyn Joshy <71560472+ashlyn2002@users.noreply.github.com>
2020-12-15 23:25:50 +0530

    display the DCLL
    create and display the DCLL

---
+++ display the DCLL.c
@@ -0,0 +1,77 @@
+/*
+ ============================================================================
+ Name        : display.c
+ Author      : ASHLYN
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void create();
+void display();
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *pre;
+};
+struct node *head=0;
+int main()
+{
+    printf("function of doubly circular linked list \n");
+    printf("1.implementation \n");
+    create();
+    printf("2.display the linked list\n");
+    display();
+
+}
+void create()
+{
+    struct node *newnode,*temp;
+    int choice;
+    while(choice)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("enter the data =");
+        scanf("%d",&newnode->data);
+        newnode->pre=0;
+        newnode->next=0;
+        if(head==0)
+        {
+            head=temp=newnode;
+            newnode->next=newnode;
+            newnode->pre=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            newnode->pre=temp;
+            newnode->next=head;
+            temp=newnode;
+            head->pre=temp;
+        }
+    printf("do you need to continue(0,1)?\nenter 0 to stop and 1 to continue =");
+    scanf("%d",&choice);
+    }
+
+}
+void display()
+{
+    struct node *temp;
+    if(head==0)
+    {
+        printf("the list is empty\n");
+    }
+    temp=head;
+    while(temp->next!=head)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("%d ",temp->data);
+    printf("\n");
+}
